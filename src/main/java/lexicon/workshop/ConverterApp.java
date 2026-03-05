02bd29b5184baf355ad01e8a1b0eadafce8b4ee0
Ragavi Gunasekaran <ragavigunasekaran@gmail.com>
2026-03-05 09:26:26 +0100

    Renamed main class name to ConverterApp

---
+++ src/main/java/lexicon/workshop/ConverterApp.java
@@ -0,0 +1,9 @@
+package lexicon.workshop;
+
+public class ConverterApp {
+    static void main() {
+
+        IO.println(String.format("Hello and welcome to my Converter Application !!!!!!"));
+
+    }
+}
