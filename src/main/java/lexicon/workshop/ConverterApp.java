e21633b362d5c3ffd3b11eaeda55506ccb21b404
Ragavi Gunasekaran <ragavigunasekaran@gmail.com>
2026-03-05 10:31:17 +0100

    Created seperate method for each conversion type and called from main method

--- src/main/java/lexicon/workshop/ConverterApp.java
+++ src/main/java/lexicon/workshop/ConverterApp.java
@@ -3,10 +3,13 @@ package lexicon.workshop;
 import java.util.Scanner;
 
 public class ConverterApp {
+
+    //Scanner class to scan the input from console
+    static Scanner scan = new Scanner(System.in);
+
     static void main() {
 
-        Scanner scan = new Scanner(System.in);
-
+        //Menu displayed in the console
         IO.println(String.format("Hello and welcome to my Converter Application !!!!!!"));
         IO.println("============== Converter App ==============");
         IO.println("1. Currency Converter");
@@ -14,20 +17,43 @@ public class ConverterApp {
         IO.println("3. Length Converter");
         IO.println("4. Exit");
         IO.println("Choose an option:");
+
+        // Get the choice of converter from the user
         int choice = scan.nextInt();
         switch (choice) {
             case 1:
                 IO.println("Currency Converter");
+                currencyConverter();
                 break;
             case 2:
                 IO.println("Temperature Converter");
+                temperatureConverter();
                 break;
             case 3:
                 IO.println("Length Converter");
+                lengthConverter();
                 break;
             case 4:
                 IO.println("Exiting the Converter Application, Thanks !!!!");
                 break;
         }
+    }
+
+    static void currencyConverter(){
+        //Currency Converter – SEK ↔ USD, SEK ↔ EUR
+        IO.println("Please enter the SEK value to convert to EURO");
+        double sekValue = scan.nextDouble();
+    }
+
+    static void temperatureConverter(){
+         //Temperature Converter – Celsius ↔ Fahrenheit
+         IO.println("Please enter the Celsius to convert to Fahrenheit");
+         int celsiusValue = scan.nextInt();
+    }
+
+    static void lengthConverter(){
+         //Length Converter – Meters ↔ Kilometers
+         IO.println("Please enter the Meters to convert to Kilometers");
+         long meterValue = scan.nextLong();
     }
 }
